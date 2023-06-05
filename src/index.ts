8bd4648711696f8f441e62921f5a0162b8743e9e
lll <17688946637@163.com>
2023-06-05 15:38:07 +0800

    feat(all):删除多余组件

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-export { default as CountDown } from './Components/CountDown';
-export { default as Dialog } from './Components/Dialog';
-export { default as Loading } from './Components/Loading';
-export { default as LoadMore } from './Components/LoadMore';
-export { default as Empty } from './Components/Empty';
 export { default as Toast } from './Components/Toast';
 export { default as Utils } from './Components/Utils';
 
